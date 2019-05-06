1e3bd8a902a39339b67a3ce3e50eea8b093d04ab
pusansong <1947028978@qq.com>
2019-05-06 22:58:27 +0800

    C语言日常代码

---
+++ 5.6.c
@@ -0,0 +1,40 @@
+//递归方式实现打印一个整数的每一位
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include<stdlib.h>
+//int print(int n) {
+//	if (n > 9) {
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//int main()
+//{
+//	int n = 0;
+//	printf("enter a number:");
+//	scanf("%d", &n);
+//	print(n);
+//	system("pause");
+//	return 0;
+//}
+//用递归实现求n的阶乘 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//int fact(int n)
+//{
+//	if (n == 1) {
+//		return 1;
+//	}
+//	return n * fact(n - 1);
+//}
+//int main()
+//
+//{
+//	int n = 0;
+//	printf("enter a number:");
+//	scanf("%d", &n);
+//	printf("%d", fact(n));
+//	system("pause");
+//	return 0;
+//}
