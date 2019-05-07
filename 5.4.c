76197029459dcad9ea8868c7eaa72c37ebd86835
pusansong <1947028978@qq.com>
2019-05-07 23:05:42 +0800

    C语言日常代码

--- 5.4.c
+++ 5.4.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //#define _CRT_SECURE_NO_WARNINGS
 //#include <stdio.h>
 //#include <stdlib.h>
@@ -19,6 +20,29 @@
 //#include <stdio.h>
 //#include <stdlib.h>
 //#include <math.h>
+=======
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//不使用（a + b） / 2这种方式，求两个数的平均值
+//float average(int a, int b) {
+//	return ((a&b) + ((a^b) >> 1));
+//
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("输入两个数:");
+//	scanf("%d,%d", &a, &b);
+//	printf("a和b的平均值等于%f", average(a, b));
+//	system("pause");
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+>>>>>>> 0965226a58c4f8f62ec7c046df783fe1c5eb1c1c
 //unsigned int reverse_bit(unsigned int value);
 //这个函数的返回值value的二进制位模式从左到右翻转后的值。
 //
@@ -28,6 +52,7 @@
 //翻转后：（2550136832）
 //10011000000000000000000000000000
 //程序结果返回：
+<<<<<<< HEAD
 //2550136832
 //
 //unsigned int reverse_bit(unsigned int value) {
@@ -49,3 +74,26 @@
 //	system("pause");
 //	return 0;
 //}
+=======
+//2550136832
+//
+//unsigned int reverse_bit(unsigned int value) {
+//	int sum = 0;
+//	int b = 0;
+//	int i;
+//	for (i = 0; i < 32; i++) {
+//		b = ((value >> i) & 1)*pow(2, (31 - i));
+//		sum += b;
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	int a = 25;
+//	int ret = 0;
+//	ret = reverse_bit(a);
+//	printf("%u\n", ret);//此处一定要用%u而不是%d的，因为若最高位为1，ret代表的是有符号数，其结果为负数
+//	system("pause");
+//	return 0;
+//}
+>>>>>>> 0965226a58c4f8f62ec7c046df783fe1c5eb1c1c
---
+++ 5.7.c
@@ -0,0 +1,48 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//:写一个递归函数DigitSum(n)，输入一个非负整数，
+//返回组成它的数字之和，
+//例如，调用DigitSum(1729)，则应该返回1 + 7 + 2 + 9，它的和是19
+//int digitnum(int n)
+//{
+//	if (n == 0)
+//	{
+//		return 0;
+//	}
+//	return n % 10 + digitnum(n / 10);
+//}
+//int main()
+//
+//{
+//	int n = 0;
+//	printf("enter a number:");
+//	scanf("%d", &n);
+//	printf("%d", digitnum(n));
+//	system("pause");
+//	return 0;
+//}
+//编写一个函数实现n^k，使用递归实现
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//double factor(int n, int k)
+//{
+//	double result = 0;
+//	if (k == 1) {
+//		return n;
+//	}
+//	result = n * factor(n, k - 1);
+//	return result;
+//}
+//int main()
+//
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d %d", &n, &k);
+//	printf("%f", factor(n, k));
+//	system("pause");
+//	return 0;
+//}
+
