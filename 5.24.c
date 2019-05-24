1ef72474b903a49ce2aa1b1e2aff43708198b3ab
pusansong <49854561+pusansong@users.noreply.github.com>
2019-05-24 20:29:56 +0800

    Add files via upload

---
+++ 5.24.c
@@ -0,0 +1,52 @@
+﻿//求两个整数的最大公约数
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a  = 0;
+//	int b  = 0;
+//	printf("输入两个整数:");
+//	scanf("%d %d", &a, &b);
+//	while (a  != b)
+//	{
+//		if (a  > b)
+//			a  = a  - b;
+//		if (b  > a)
+//			b  = b  - a;
+//	}
+//	printf("%d ", a);
+//	system("pause");
+//	return 0;
+//}
+//按从小到大的顺序输出三个整数
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a, b, c, i;
+//	printf("输入三个整数:");
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a  > b)
+//	{
+//		i  = b;
+//		b  = a;
+//		a  = i;
+//	}
+//	if (a  > c)
+//	{
+//		i  = c;
+//		c  = a;
+//		a  = i;
+//	}
+//	if (b  > c)
+//	{
+//		i  = c;
+//		c  = b;
+//		b  = i;
+//	}
+//	printf("%d %d %d", a, b, c);
+//	system("pause");
+//	return 0;
+//}
